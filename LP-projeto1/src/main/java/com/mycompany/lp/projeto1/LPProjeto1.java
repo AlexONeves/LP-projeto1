8c1b7ac76ea84e63f7c0f4d54f041e29b1307c02
AlexONeves <duartenevesalexandre@ua.pt>
2023-03-30 09:26:53 +0100

    class Aluno e Turma, com print da turma toda

--- LP-projeto1/src/main/java/com/mycompany/lp/projeto1/LPProjeto1.java
+++ LP-projeto1/src/main/java/com/mycompany/lp/projeto1/LPProjeto1.java
@@ -4,11 +4,31 @@
 package com.mycompany.lp.projeto1;
 
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author alex
  */
+class Turma {
+
+    private List<Aluno> alunos;
+
+    public Turma() {
+        this.alunos = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            Aluno al = new Aluno();
+            alunos.add(al);
+        }
+    }
+
+    public List getTurma() {
+        return alunos;
+    }
+}
+
 class Aluno {
 
     private int[] respostas = new int[20];
@@ -38,14 +58,18 @@ class Aluno {
 public class LPProjeto1 {
 
     public static void main(String[] args) {
-        Aluno al = new Aluno();
-        int[] respostas = al.getRespostas();
-
-        // print the values of the respostas array
-        System.out.println("Respostas:");
-        for (int i = 0; i < respostas.length; i++) {
-            System.out.print(respostas[i] + " ");
+        Turma turma = new Turma();
+        List<Aluno> alunos = turma.getTurma(); // get Lista
+        int j = 1;
+        for (Aluno aluno : alunos) { // Por cada Aluno na Lista de Alunos
+            int[] respostas = aluno.getRespostas(); // get Respostas de Cada Aluno
+            System.out.print("Respostas do aluno " + j + " : ");
+            for (int i = 0; i < respostas.length; i++) {
+                System.out.print(respostas[i] + " ");
+            }
+            System.out.println();
+            j++;
         }
-        System.out.println();
+
     }
 }
